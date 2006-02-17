b1a1f0b067afe562169b928d53eb96f37d195efe
Quietust <quietust@gmail.com>
2006-02-17 22:47:56 +0000

    Actually check in the config dialogs

--- src/resource.h
+++ src/resource.h
@@ -126,7 +126,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        144
 #define _APS_NEXT_COMMAND_VALUE         32809
-#define _APS_NEXT_CONTROL_VALUE         1056
+#define _APS_NEXT_CONTROL_VALUE         1059
 #define _APS_NEXT_SYMED_VALUE           109
 #endif
 #endif
