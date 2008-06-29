5e7e7b4563a7c4d4f1e7f2c6e3d7f8d054c17f9d
Quietust <quietust@gmail.com>
2008-06-29 20:16:02 +0000

    * Update copyright year to 2008 * Add URL/ID properties to source files for revision tracking * Make all source files use native line endings * GPL notice really only needs to be on the main source files - remove it from other files, leaving a shortened header

--- src/Debugger.c
+++ src/Debugger.c
@@ -1,24 +1,9 @@
-/*
-Nintendulator - A Win32 NES emulator written in C.
-Designed for maximum emulation accuracy.
-Copyright (c) 2002-2006  QMT Productions
-
-Based on NinthStar, a portable Win32 NES Emulator written in C++
-Copyright (C) 2000  David de Regt
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-For a copy of the GNU General Public License, go to:
-http://www.gnu.org/copyleft/gpl.html#SEC1
-*/
+/* Nintendulator - Win32 NES emulator written in C
+ * Copyright (C) 2002-2008 QMT Productions
+ *
+ * $URL$
+ * $Id$
+ */
 
 #include "stdafx.h"
 #include <time.h>
