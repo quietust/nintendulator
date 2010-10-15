5a29f22921d7682595c1b4de632d8943d017f644
Quietust <quietust@gmail.com>
2010-10-15 04:57:24 +0000

    Add movie support for Family Basic Keyboard and Alternate (Dendy?) Keyboard

--- src/c_e_altkey.cpp
+++ src/c_e_altkey.cpp
@@ -16,18 +16,55 @@ namespace Controllers
 #include <pshpack1.h>
 struct ExpPort_AltKeyboard_State
 {
-	unsigned char Out;
-	unsigned char Scan;
+	unsigned char Column;
+	unsigned char Row;
+	unsigned char Keys[13];
 };
 #include <poppack.h>
 #define State ((ExpPort_AltKeyboard_State *)Data)
 
 void	ExpPort_AltKeyboard::Frame (unsigned char mode)
 {
+	int row, col;
+	if (mode & MOV_PLAY)
+	{
+		for (row = 0; row < 13; row++)
+			State->Keys[row] = MovData[row];
+	}
+	else
+	{
+		const int keymap[13][8] = {
+			{DIK_4, DIK_G, DIK_F, DIK_C, DIK_F2, DIK_E, DIK_5, DIK_V},
+			{DIK_2, DIK_D, DIK_S, DIK_END, DIK_F1, DIK_W, DIK_3, DIK_X},
+			{DIK_INSERT, DIK_BACKSPACE, DIK_PGDN, DIK_RIGHT, DIK_F8, DIK_PGUP, DIK_DELETE, DIK_HOME},
+			{DIK_9, DIK_I, DIK_L, DIK_COMMA, DIK_F5, DIK_O, DIK_0, DIK_PERIOD},
+			{DIK_RBRACKET, DIK_RETURN, DIK_UP, DIK_LEFT, DIK_F7, DIK_LBRACKET, DIK_BACKSLASH, DIK_DOWN},
+			{DIK_Q, DIK_CAPITAL, DIK_Z, DIK_SYSRQ, DIK_ESCAPE, DIK_A, DIK_1, DIK_LCONTROL},
+			{DIK_7, DIK_Y, DIK_K, DIK_M, DIK_F4, DIK_U, DIK_8, DIK_J},
+			{DIK_MINUS, DIK_SEMICOLON, DIK_APOSTROPHE, DIK_CAPITAL, DIK_F6, DIK_P, DIK_EQUALS, DIK_LSHIFT},
+			{DIK_T, DIK_H, DIK_N, DIK_SPACE, DIK_F3, DIK_R, DIK_6, DIK_B},
+
+			{DIK_GRAVE, DIK_F10, DIK_F11, DIK_F12, DIK_ADD, DIK_SUBTRACT, DIK_MULTIPLY, DIK_DIVIDE},
+			{DIK_SCROLL, DIK_PAUSE, DIK_GRAVE, DIK_TAB, DIK_NUMPAD6, DIK_NUMPAD7, DIK_NUMPAD8, DIK_NUMPAD9},
+			{DIK_CAPITAL, DIK_SLASH, DIK_RSHIFT, DIK_LMENU, DIK_NUMPAD0, DIK_ADD, DIK_SUBTRACT, DIK_MULTIPLY},
+			{DIK_RMENU, DIK_APPS, DIK_RCONTROL, DIK_NUMPAD1, DIK_DIVIDE, DIK_DECIMAL, DIK_NUMLOCK, DIK_NUMPADENTER}
+		};
+
+		for (row = 0; row < 13; row++)
+		{
+			State->Keys[row] = 0;
+			for (col = 0; col < 8; col++)
+				if (KeyState[keymap[row][col]] & 0x80)
+					State->Keys[row] |= 1 << col;
+		}
+		// special case
+		if (KeyState[DIK_RSHIFT] & 0x80)
+			State->Keys[7] |= 0x80;
+	}
 	if (mode & MOV_RECORD)
 	{
-		MessageBox(hMainWnd, _T("Alternate Famicom Keyboard does not support recording movies!"), _T("Nintendulator"), MB_OK | MB_ICONERROR);
-		Movie::Stop();
+		for (row = 0; row < 13; row++)
+			MovData[row] = State->Keys[row];
 	}
 }
 unsigned char	ExpPort_AltKeyboard::Read1 (void)
@@ -37,147 +74,11 @@ unsigned char	ExpPort_AltKeyboard::Read1 (void)
 unsigned char	ExpPort_AltKeyboard::Read2 (void)
 {
 	unsigned char result = 0;
-	if (State->Out == 0)
+	if (State->Row < 13)
 	{
-		switch (State->Scan)
-		{
-		case 0:	if (KeyState[DIK_4] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_G] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_F] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_C] & 0x80)	result |= 0x10;
-			break;
-		case 1:	if (KeyState[DIK_2] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_D] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_S] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_END] & 0x80)	result |= 0x10;
-			break;
-		case 2:	if (KeyState[DIK_INSERT] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_BACKSPACE] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_PGDN] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_RIGHT] & 0x80)		result |= 0x10;
-			break;
-		case 3:	if (KeyState[DIK_9] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_I] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_L] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_COMMA] & 0x80)	result |= 0x10;
-			break;
-		case 4:	if (KeyState[DIK_RBRACKET] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_RETURN] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_UP] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_LEFT] & 0x80)		result |= 0x10;
-			break;
-		case 5:	if (KeyState[DIK_Q] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_CAPITAL] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_Z] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_SYSRQ] & 0x80)		result |= 0x10;
-			break;
-		case 6:	if (KeyState[DIK_7] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_Y] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_K] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_M] & 0x80)	result |= 0x10;
-			break;
-		case 7:	if (KeyState[DIK_MINUS] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_SEMICOLON] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_APOSTROPHE] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_CAPITAL] & 0x80)	result |= 0x10;
-			break;
-		case 8:	if (KeyState[DIK_T] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_H] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_N] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_SPACE] & 0x80)	result |= 0x10;
-			break;
-		case 9: if (KeyState[DIK_GRAVE] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_F10] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_F11] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_F12] & 0x80)	result |= 0x10;
-			break;
-		case 10:if (KeyState[DIK_SCROLL] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_PAUSE] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_GRAVE] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_TAB] & 0x80)		result |= 0x10;
-			break;
-		case 11:if (KeyState[DIK_CAPITAL] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_SLASH] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_RSHIFT] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_LMENU] & 0x80)		result |= 0x10;
-			break;
-		case 12:if (KeyState[DIK_RMENU] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_APPS] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_RCONTROL] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_NUMPAD1] & 0x80)	result |= 0x10;
-		}
-	}
-	else
-	{
-		switch (State->Scan)
-		{
-		case 0:	if (KeyState[DIK_F2] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_E] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_5] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_V] & 0x80)	result |= 0x10;
-			break;
-		case 1:	if (KeyState[DIK_F1] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_W] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_3] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_X] & 0x80)	result |= 0x10;
-			break;
-		case 2:	if (KeyState[DIK_F8] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_PGUP] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_DELETE] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_HOME] & 0x80)		result |= 0x10;
-			break;
-		case 3:	if (KeyState[DIK_F5] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_O] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_0] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_PERIOD] & 0x80)	result |= 0x10;
-			break;
-		case 4:	if (KeyState[DIK_F7] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_LBRACKET] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_BACKSLASH] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_DOWN] & 0x80)		result |= 0x10;
-			break;
-		case 5:	if (KeyState[DIK_ESCAPE] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_A] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_1] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_LCONTROL] & 0x80)	result |= 0x10;
-			break;
-		case 6:	if (KeyState[DIK_F4] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_U] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_8] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_J] & 0x80)	result |= 0x10;
-			break;
-		case 7:	if (KeyState[DIK_F6] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_P] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_EQUALS] & 0x80)	result |= 0x08;
-			if ((KeyState[DIK_LSHIFT] & 0x80) || (KeyState[DIK_RSHIFT] & 0x80))
-								result |= 0x10;
-			break;
-		case 8:	if (KeyState[DIK_F3] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_R] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_6] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_B] & 0x80)	result |= 0x10;
-			break;
-		case 9:	if (KeyState[DIK_ADD] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_SUBTRACT] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_MULTIPLY] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_DIVIDE] & 0x80)	result |= 0x10;
-			break;
-		case 10:if (KeyState[DIK_NUMPAD6] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_NUMPAD7] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_NUMPAD8] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_NUMPAD9] & 0x80)	result |= 0x10;
-			break;
-		case 11:if (KeyState[DIK_NUMPAD0] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_ADD] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_SUBTRACT] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_MULTIPLY] & 0x80)	result |= 0x10;
-			break;
-		case 12:if (KeyState[DIK_DIVIDE] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_DECIMAL] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_NUMLOCK] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_NUMPADENTER] & 0x80)	result |= 0x10;
-			break;
-		}
+		if (State->Column)
+			result = (State->Keys[State->Row] & 0xF0) >> 3;
+		else	result = (State->Keys[State->Row] & 0x0F) << 1;
 	}
 	return result ^ 0x1E;
 }
@@ -185,15 +86,15 @@ unsigned char	ExpPort_AltKeyboard::Read2 (void)
 void	ExpPort_AltKeyboard::Write (unsigned char Val)
 {
 	BOOL ResetKB = Val & 1;
-	BOOL WhichScan = Val & 2;
+	BOOL SelColumn = Val & 2;
 	BOOL SelectKey = Val & 4;
 	if (SelectKey)
 	{
-		if ((State->Out) && (!WhichScan))
-			State->Scan++;
-		State->Out = WhichScan;
+		if ((State->Column) && (!SelColumn))
+			State->Row++;
+		State->Column = SelColumn;
 		if (ResetKB)
-			State->Scan = 0;
+			State->Row = 0;
 	}
 	else
 	{
@@ -216,10 +117,11 @@ ExpPort_AltKeyboard::ExpPort_AltKeyboard (int *buttons)
 	Buttons = buttons;
 	DataLen = sizeof(*State);
 	Data = malloc(DataLen);
-	MovLen = 0;
+	MovLen = 13;
 	MovData = (unsigned char *)malloc(MovLen);
 	ZeroMemory(MovData, MovLen);
-	State->Out = 0;
-	State->Scan = 0;
+	State->Row = 0;
+	State->Column = 0;
+	ZeroMemory(State->Keys, sizeof(State->Keys));
 }
 } // namespace Controllers
--- src/c_e_fbkey.cpp
+++ src/c_e_fbkey.cpp
@@ -16,18 +16,50 @@ namespace Controllers
 #include <pshpack1.h>
 struct ExpPort_FamilyBasicKeyboard_State
 {
-	unsigned char Out;
-	unsigned char Scan;
+	unsigned char Column;
+	unsigned char Row;
+	unsigned char Keys[9];
 };
 #include <poppack.h>
 #define State ((ExpPort_FamilyBasicKeyboard_State *)Data)
 
 void	ExpPort_FamilyBasicKeyboard::Frame (unsigned char mode)
 {
+	int row, col;
+	if (mode & MOV_PLAY)
+	{
+		for (row = 0; row < 9; row++)
+			State->Keys[row] = MovData[row];
+	}
+	else
+	{
+		const int keymap[9][8] = {
+			{DIK_F8, DIK_RETURN, DIK_LBRACKET, DIK_RBRACKET, DIK_CAPITAL /* KANA */, DIK_RSHIFT, DIK_BACKSLASH, DIK_END /* STOP */},
+			{DIK_F7, DIK_GRAVE /* @ */, DIK_APOSTROPHE /* : */, DIK_SEMICOLON, DIK_HOME /* _ */, DIK_SLASH, DIK_MINUS, DIK_EQUALS /* ^ */},
+			{DIK_F6, DIK_O, DIK_L, DIK_K, DIK_PERIOD, DIK_COMMA, DIK_P, DIK_0},
+			{DIK_F5, DIK_I, DIK_U, DIK_J, DIK_M, DIK_N, DIK_9, DIK_8},
+			{DIK_F4, DIK_Y, DIK_G, DIK_H, DIK_B, DIK_V, DIK_7, DIK_6},
+			{DIK_F3, DIK_T, DIK_R, DIK_D, DIK_F, DIK_C, DIK_5, DIK_4},
+			{DIK_F2, DIK_W, DIK_S, DIK_A, DIK_X, DIK_Z, DIK_E, DIK_3},
+			{DIK_F1, DIK_ESCAPE, DIK_Q, DIK_LCONTROL, DIK_LSHIFT, DIK_TAB /* GRPH */, DIK_1, DIK_2},
+			{DIK_DELETE /* CLR */, DIK_UP, DIK_RIGHT, DIK_LEFT, DIK_DOWN, DIK_SPACE, DIK_BACK /* DEL, actually backspace */, DIK_INSERT}
+		};
+
+		for (row = 0; row < 9; row++)
+		{
+			State->Keys[row] = 0;
+			for (col = 0; col < 8; col++)
+				if (KeyState[keymap[row][col]] & 0x80)
+					State->Keys[row] |= 1 << col;
+		}
+		// special case - allow either Ctrl key
+		if (KeyState[DIK_RCONTROL] & 0x80)
+			State->Keys[7] |= 0x08;
+	}
 	if (mode & MOV_RECORD)
 	{
-		MessageBox(hMainWnd, _T("Family Basic Keyboard does not support recording movies!"), _T("Nintendulator"), MB_OK | MB_ICONERROR);
-		Movie::Stop();
+		for (row = 0; row < 9; row++)
+			MovData[row] = State->Keys[row];
 	}
 }
 unsigned char	ExpPort_FamilyBasicKeyboard::Read1 (void)
@@ -37,126 +69,23 @@ unsigned char	ExpPort_FamilyBasicKeyboard::Read1 (void)
 unsigned char	ExpPort_FamilyBasicKeyboard::Read2 (void)
 {
 	unsigned char result = 0;
-	if (State->Out == 0)
-	{
-		switch (State->Scan)
-		{
-		case 0:	if (KeyState[DIK_F8] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_RETURN] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_LBRACKET] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_RBRACKET] & 0x80)	result |= 0x10;
-			break;
-		case 1:	if (KeyState[DIK_F7] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_GRAVE] & 0x80)		result |= 0x04;	// @
-			if (KeyState[DIK_APOSTROPHE] & 0x80)	result |= 0x08;	// :
-			if (KeyState[DIK_SEMICOLON] & 0x80)	result |= 0x10;
-			break;
-		case 2:	if (KeyState[DIK_F6] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_O] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_L] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_K] & 0x80)	result |= 0x10;
-			break;
-		case 3:	if (KeyState[DIK_F5] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_I] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_U] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_J] & 0x80)	result |= 0x10;
-			break;
-		case 4:	if (KeyState[DIK_F4] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_Y] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_G] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_H] & 0x80)	result |= 0x10;
-			break;
-		case 5:	if (KeyState[DIK_F3] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_T] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_R] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_D] & 0x80)	result |= 0x10;
-			break;
-		case 6:	if (KeyState[DIK_F2] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_W] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_S] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_A] & 0x80)	result |= 0x10;
-			break;
-		case 7:	if (KeyState[DIK_F1] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_ESCAPE] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_Q] & 0x80)		result |= 0x08;
-			if ((KeyState[DIK_LCONTROL] & 0x80) || (KeyState[DIK_RCONTROL] & 0x80))
-								result |= 0x10;
-			break;
-		case 8:	if (KeyState[DIK_BACK] & 0x80)	result |= 0x02;	// CLR
-			if (KeyState[DIK_UP] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_RIGHT] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_LEFT] & 0x80)	result |= 0x10;
-			break;
-		}
-	}
-	else
-	{
-		switch (State->Scan)
-		{
-		case 0:	if (KeyState[DIK_CAPITAL] & 0x80)	result |= 0x02;	// KANA
-			if (KeyState[DIK_RSHIFT] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_BACKSLASH] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_END] & 0x80)		result |= 0x10;	// STOP
-			break;
-		case 1:	if (KeyState[DIK_HOME] & 0x80)		result |= 0x02;	// _
-			if (KeyState[DIK_SLASH] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_MINUS] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_EQUALS] & 0x80)	result |= 0x10; // ^
-			break;
-		case 2:	if (KeyState[DIK_PERIOD] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_COMMA] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_P] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_0] & 0x80)		result |= 0x10;
-			break;
-		case 3:	if (KeyState[DIK_M] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_N] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_9] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_8] & 0x80)	result |= 0x10;
-			break;
-		case 4:	if (KeyState[DIK_B] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_V] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_7] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_6] & 0x80)	result |= 0x10;
-			break;
-		case 5:	if (KeyState[DIK_F] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_C] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_5] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_4] & 0x80)	result |= 0x10;
-			break;
-		case 6:	if (KeyState[DIK_X] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_Z] & 0x80)	result |= 0x04;
-			if (KeyState[DIK_E] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_3] & 0x80)	result |= 0x10;
-			break;
-		case 7:	if (KeyState[DIK_LSHIFT] & 0x80)	result |= 0x02;
-			if (KeyState[DIK_TAB] & 0x80)		result |= 0x04;	// GRPH
-			if (KeyState[DIK_1] & 0x80)		result |= 0x08;
-			if (KeyState[DIK_2] & 0x80)		result |= 0x10;
-			break;
-		case 8:	if (KeyState[DIK_DOWN] & 0x80)		result |= 0x02;
-			if (KeyState[DIK_SPACE] & 0x80)		result |= 0x04;
-			if (KeyState[DIK_DELETE] & 0x80)	result |= 0x08;
-			if (KeyState[DIK_INSERT] & 0x80)	result |= 0x10;
-			break;
-		}
-	}
+	if (State->Column)
+		result = (State->Keys[State->Row] & 0xF0) >> 3;
+	else	result = (State->Keys[State->Row] & 0x0F) << 1;
 	return result ^ 0x1E;
 }
 void	ExpPort_FamilyBasicKeyboard::Write (unsigned char Val)
 {
 	BOOL ResetKB = Val & 1;
-	BOOL WhichScan = Val & 2;
+	BOOL SelColumn = Val & 2;
 	BOOL SelectKey = Val & 4;
 	if (SelectKey)
 	{
-		if ((State->Out) && (!WhichScan))
-		{
-			State->Scan++;
-			State->Scan %= 10;
-		}
-		State->Out = WhichScan;
+		if ((State->Column) && (!SelColumn))
+			State->Row = (State->Row + 1) % 10;
+		State->Column = SelColumn;
 		if (ResetKB)
-			State->Scan = 0;
+			State->Row = 0;
 	}
 	else
 	{
@@ -205,10 +134,11 @@ ExpPort_FamilyBasicKeyboard::ExpPort_FamilyBasicKeyboard (int *buttons)
 	Buttons = buttons;
 	DataLen = sizeof(*State);
 	Data = malloc(DataLen);
-	MovLen = 0;
+	MovLen = 9;
 	MovData = (unsigned char *)malloc(MovLen);
 	ZeroMemory(MovData, MovLen);
-	State->Out = 0;
-	State->Scan = 0;
+	State->Column = 0;
+	State->Row = 0;
+	ZeroMemory(State->Keys, sizeof(State->Keys));
 }
 } // namespace Controllers
